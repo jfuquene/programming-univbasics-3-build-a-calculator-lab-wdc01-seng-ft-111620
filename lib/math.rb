bd8a431368efa7635487d88e4265881922d7de03
John Fuquene <jfuquene@email.wm.edu>
2020-11-05 20:35:41 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,46 @@
 def addition(num1, num2)
-
+  puts "#{num1}" 
+  puts "#{num2}"
+  sum = num1 + num2 
+  puts = sum 
+  sum 
 end
 
 def subtraction(num1, num2)
-
+  puts "#{num1}"
+  puts "#{num2}"
+  subtract = num1 - num2 
+  puts = subtract
+  subtract
 end
 
 def division(num1, num2)
-
+  puts "#{num1}"
+  puts "#{num2}"
+  division = num1 / num2 
+  puts = division 
+  division 
 end
 
 def multiplication(num1, num2)
-
+  puts "#{num1}"
+  puts "#{num2}"
+  multiply = num1 * num2
+  puts multiply 
+  multiply
 end
 
 def modulo(num1, num2)
-
+  puts "#{num1}"
+  puts "#{num2}"
+  modulo = num1 % num2 
+  puts modulo
+  modulo
 end
 
 def square_root(num)
-
+  puts Math.sqrt(num)
+  square = Math.sqrt(num)
+  puts = square
+  square
 end
